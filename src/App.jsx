c78cd546f8809fd8bba0816e25e900610961db21
tkahmedkamal <tkahmedkamal@hotmail.com>
2022-11-13 09:55:04 +0200

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">App Component</div>;
+}
+
+export default App;
---
+++ src/apis/baseUrl.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Accept-Language': 'en-US',
+  },
+});
+
+export default api;
